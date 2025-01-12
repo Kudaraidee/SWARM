0806d778daab4b69257e6a0eff8025d22bc0f80b
Decho Kocharin <sourcedecho@outlook.com>
2025-01-12 09:55:22 +0700

    Update wildrig-n.ps1

--- miners/gpu/nvidia/wildrig-n.ps1
+++ miners/gpu/nvidia/wildrig-n.ps1
@@ -51,7 +51,7 @@ $(vars).NVIDIATypes | ForEach-Object {
     $MinerConfig = $Global:config.miners.$ref
     
     ##Export would be /path/to/[SWARMVERSION]/build/export##
-    $ExportDir = "/usr/local/swarm/lib64"
+    $ExportDir = "/lib/x86_64-linux-gnu"
     $Miner_Dir = Join-Path ($(vars).dir) ((Split-Path $Path).replace(".", ""))
 
     ##Prestart actions before miner launch
