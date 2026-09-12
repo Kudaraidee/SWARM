3e067d725f92ff9902ce2ca099999a366012c1b0
Decho Kocharin <sourcedecho@outlook.com>
2026-09-12 22:57:53 +0700

    Update cc-tpfuemp.ps1

--- miners/gpu/nvidia/cc-tpfuemp.ps1
+++ miners/gpu/nvidia/cc-tpfuemp.ps1
@@ -11,7 +11,7 @@ $(vars).NVIDIATypes | ForEach-Object {
     if ($(vars).nvidia.$CName.uri) { $Uri = "$($(vars).nvidia.$CName.uri)" } else { $Uri = "None" }
     if ($(vars).nvidia.$CName.minername) { $MinerName = "$($(vars).nvidia.$CName.minername)" } else { $MinerName = "None" }
 
-    $User = "User$Num"; $Pass = "Pass$Num"; $Name = "$CName-$Num"; $Port = "4100$Num"
+    $User = "User$Num"; $Pass = "Pass$Num"; $Name = "$CName-$Num"; $Port = "4200$Num"
 
     Switch ($Num) {
         1 { $Get_Devices = $(vars).NVIDIADevices1; $Rig = $(arg).RigName1 }
@@ -47,57 +47,59 @@ $(vars).NVIDIATypes | ForEach-Object {
     if ($(vars).Bancount -lt 1) { $(vars).Bancount = 5 }
 
     ##Build Miner Settings
-    $MinerConfig.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    if ($null -ne $MinerConfig.$ConfigType.commands) {
+        $MinerConfig.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
 
-        $MinerAlgo = $_
+            $MinerAlgo = $_
 
-        if (
-            $MinerAlgo -in $MinerAlgos -and
-            $Name -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
-            $ConfigType -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
-            $Name -notin $(vars).BanHammer
-        ) {
-            $StatAlgo = $MinerAlgo -replace "`_", "`-"
-            $Stat = Global:Get-Stat -Name "$($Name)_$($StatAlgo)_hashrate"
-            if ($(arg).Rej_Factor -eq "Yes" -and $Stat.Rejections -gt 0 -and $Stat.Rejection_Periods -ge 3) { $HashStat = $Stat.Hour * (1 - ($Stat.Rejections * 0.01)) }
-            else { $HashStat = $Stat.Hour }
-            $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
-                $Diff = ""
-                if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { 
-                    switch($_.Name) {
-                        "zergpool" { $Diff = ",sd=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
-                        default { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+            if (
+                $MinerAlgo -in $MinerAlgos -and
+                $Name -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
+                $ConfigType -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
+                $Name -notin $(vars).BanHammer
+            ) {
+                $StatAlgo = $MinerAlgo -replace "`_", "`-"
+                $Stat = Global:Get-Stat -Name "$($Name)_$($StatAlgo)_hashrate"
+                if ($(arg).Rej_Factor -eq "Yes" -and $Stat.Rejections -gt 0 -and $Stat.Rejection_Periods -ge 3) { $HashStat = $Stat.Hour * (1 - ($Stat.Rejections * 0.01)) }
+                else { $HashStat = $Stat.Hour }
+                $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
+                    $Diff = ""
+                    if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) {  
+                        switch($_.Name) {
+                            "zergpool" { $Diff = ",sd=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+                            default { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+                        }
                     }
-                }
-                [PSCustomObject]@{
-                    MName      = $Name
-                    Coin       = $(vars).Coins
-                    Delay      = $MinerConfig.$ConfigType.delay
-                    Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
-                    Symbol     = "$($_.Symbol)"
-                    MinerName  = $MinerName
-                    Prestart   = $PreStart
-                    Type       = $ConfigType
-                    Path       = $Path
-                    Devices    = $Devices
-                    Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)"
-                    Version    = "$($(vars).nvidia.$CName.version)"
-                    DeviceCall = "ccminer"
-                    Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Pool_Host):$($_.Port) -b 0.0.0.0:$Port -u $($_.$User) -p $($_.$Pass)$($Diff)$($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
-                    HashRates  = [Decimal]$Stat.Hour
-                    HashRate_Adjusted = [Decimal]$Hashstat
-                    Quote      = $_.Price
-                    Rejections = $Stat.Rejections
-                    Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 }
-                    MinerPool  = "$($_.Name)"
-                    Port       = $Port
-                    Worker     = $Rig
-                    API        = "Ccminer"
-                    Wallet     = "$($_.$User)"
-                    URI        = $Uri
-                    Server     = "localhost"
-                    Algo       = "$($_.Algorithm)"
-                    Log        = $Log
+                    [PSCustomObject]@{
+                        MName      = $Name
+                        Coin       = $(vars).Coins
+                        Delay      = $MinerConfig.$ConfigType.delay
+                        Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
+                        Symbol     = "$($_.Symbol)"
+                        MinerName  = $MinerName
+                        Prestart   = $PreStart
+                        Type       = $ConfigType
+                        Path       = $Path
+                        Devices    = $Devices
+                        Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)"
+                        Version    = "$($(vars).nvidia.$CName.version)"
+                        DeviceCall = "ccminer"
+                        Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Pool_Host):$($_.Port) -b 0.0.0.0:$Port -u $($_.$User) -p $($_.$Pass)$($Diff)$($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates  = [Decimal]$Stat.Hour
+                        HashRate_Adjusted = [Decimal]$Hashstat
+                        Quote      = $_.Price
+                        Rejections = $Stat.Rejections
+                        Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 }
+                        MinerPool  = "$($_.Name)"
+                        Port       = $Port
+                        Worker     = $Rig
+                        API        = "Ccminer"
+                        Wallet     = "$($_.$User)"
+                        URI        = $Uri
+                        Server     = "localhost"
+                        Algo       = "$($_.Algorithm)"
+                        Log        = $Log
+                    }
                 }
             }
         }
