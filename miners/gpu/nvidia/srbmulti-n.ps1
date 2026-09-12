790599a0af01d184721328dca43083519055c005
Decho Kocharin <sourcedecho@outlook.com>
2026-09-12 23:13:12 +0700

    Update srbmulti-n.ps1

--- miners/gpu/nvidia/srbmulti-n.ps1
+++ miners/gpu/nvidia/srbmulti-n.ps1
@@ -50,61 +50,63 @@ $(vars).NVIDIATypes | ForEach-Object {
     if ($(vars).Bancount -lt 1) { $(vars).Bancount = 5 }
 
     ##Build Miner Settings
-    $MinerConfig.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    if ($null -ne $MinerConfig.$ConfigType.commands) {
+        $MinerConfig.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
 
-        $MinerAlgo = $_
+            $MinerAlgo = $_
 
-        if (
-            $MinerAlgo -in $MinerAlgos -and
-            $Name -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
-            $ConfigType -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
-            $Name -notin $(vars).BanHammer
-        ) {
-            $StatAlgo = $MinerAlgo -replace "`_", "`-"
-            $Stat = Global:Get-Stat -Name "$($Name)_$($StatAlgo)_hashrate"
-            if ($(arg).Rej_Factor -eq "Yes" -and $Stat.Rejections -gt 0 -and $Stat.Rejection_Periods -ge 3) { $HashStat = $Stat.Hour * (1 - ($Stat.Rejections * 0.01)) }
-            else { $HashStat = $Stat.Hour }
-            $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
-                $Nicehash = ""
-                if($_.Name -eq "Nicehash") {
-                    $Nicehash = "--Nicehash true "
-                }
-                $Diff = ""
-                if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { 
-                    switch($_.Name) {
-                        "zergpool" { $Diff = ",sd=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
-                        default { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+            if (
+                $MinerAlgo -in $MinerAlgos -and
+                $Name -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
+                $ConfigType -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and
+                $Name -notin $(vars).BanHammer
+            ) {
+                $StatAlgo = $MinerAlgo -replace "`_", "`-"
+                $Stat = Global:Get-Stat -Name "$($Name)_$($StatAlgo)_hashrate"
+                if ($(arg).Rej_Factor -eq "Yes" -and $Stat.Rejections -gt 0 -and $Stat.Rejection_Periods -ge 3) { $HashStat = $Stat.Hour * (1 - ($Stat.Rejections * 0.01)) }
+                else { $HashStat = $Stat.Hour }
+                $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
+                    $Nicehash = ""
+                    if($_.Name -eq "Nicehash") {
+                        $Nicehash = "--Nicehash true "
                     }
-                }
-                [PSCustomObject]@{
-                    MName      = $Name
-                    Coin       = $(vars).Coins
-                    Delay      = $MinerConfig.$ConfigType.delay
-                    Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
-                    Symbol     = "$($_.Symbol)"
-                    MinerName  = $MinerName
-                    Prestart   = $PreStart
-                    Type       = $ConfigType
-                    Path       = $Path
-                    Devices    = $Devices
-                    Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)"
-                    Version    = "$($(vars).nvidia.$ref.version)"
-                    DeviceCall = "srbminer"
-                    Arguments  = "$Nicehash--adl-disable --gpu-platform $($(vars).NVIDIAPlatform) --disable-cpu --algorithm $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) --pool $($_.Pool_Host):$($_.Port) --wallet $($_.$User) --password $($_.$Pass)$Diff --api-enable --logfile `'$Log`' --api-port $Port $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
-                    HashRates  = [Decimal]$Stat.Hour
-                    HashRate_Adjusted = [Decimal]$Hashstat
-                    Quote      = $_.Price
-                    Rejections = $Stat.Rejections
-                    Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 }
-                    MinerPool  = "$($_.Name)"
-                    Port       = $Port
-                    Worker     = $Rig
-                    API        = "srbmulti"
-                    Wallet     = "$($_.$User)"
-                    URI        = $Uri
-                    Server     = "localhost"
-                    Algo       = "$($_.Algorithm)"
-                    Log        = "miner_generated"
+                    $Diff = ""
+                    if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) {  
+                        switch($_.Name) {
+                            "zergpool" { $Diff = ",sd=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+                            default { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
+                        }
+                    }
+                    [PSCustomObject]@{
+                        MName      = $Name
+                        Coin       = $(vars).Coins
+                        Delay      = $MinerConfig.$ConfigType.delay
+                        Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
+                        Symbol     = "$($_.Symbol)"
+                        MinerName  = $MinerName
+                        Prestart   = $PreStart
+                        Type       = $ConfigType
+                        Path       = $Path
+                        Devices    = $Devices
+                        Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)"
+                        Version    = "$($(vars).nvidia.$ref.version)"
+                        DeviceCall = "srbminer"
+                        Arguments  = "$Nicehash--adl-disable --gpu-platform $($(vars).NVIDIAPlatform) --disable-cpu --algorithm $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) --pool $($_.Pool_Host):$($_.Port) --wallet $($_.$User) --password $($_.$Pass)$Diff --api-enable --logfile `'$Log`' --api-port $Port $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates  = [Decimal]$Stat.Hour
+                        HashRate_Adjusted = [Decimal]$Hashstat
+                        Quote      = $_.Price
+                        Rejections = $Stat.Rejections
+                        Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 }
+                        MinerPool  = "$($_.Name)"
+                        Port       = $Port
+                        Worker     = $Rig
+                        API        = "srbmulti"
+                        Wallet     = "$($_.$User)"
+                        URI        = $Uri
+                        Server     = "localhost"
+                        Algo       = "$($_.Algorithm)"
+                        Log        = "miner_generated"
+                    }
                 }
             }
         }
