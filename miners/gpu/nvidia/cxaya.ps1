ee060ff04ddf2d9ebfb2325ff315e3feafbcf03c
Decho Kocharin <sourcedecho@outlook.com>
2026-09-12 19:41:02 +0700

    Update cxaya.ps1

--- miners/gpu/nvidia/cxaya.ps1
+++ miners/gpu/nvidia/cxaya.ps1
@@ -34,7 +34,7 @@ $(vars).NVIDIATypes | ForEach-Object {
     $MinerConfig = $Global:config.miners.'cxaya'
 
     ##Export would be /path/to/[SWARMVERSION]/build/export##
-    $ExportDir = "/lib/x86_64-linux-gnu"
+    $ExportDir = "/usr/local/swarm/lib64"
     $Miner_Dir = Join-Path ($(vars).dir) ((Split-Path $Path).replace(".", ""))
 
     ##Prestart actions before miner launch
